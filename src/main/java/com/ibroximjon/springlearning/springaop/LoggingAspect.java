851c4d55ab17349361cfc274423612d79c308232
Ibroximjon <makhammadjonovibrokhimjon@gmail.com>
2025-02-24 12:20:22 +0500

    aop functions are checked

--- src/main/java/com/ibroximjon/springlearning/springaop/LoggingAspect.java
+++ src/main/java/com/ibroximjon/springlearning/springaop/LoggingAspect.java
@@ -1,9 +1,8 @@
 package com.ibroximjon.springlearning.springaop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -19,4 +18,36 @@ public class LoggingAspect {
     public void afterPaymentMethod() {
         System.out.println("Payment method end");
     }
+
+    @AfterReturning(
+            pointcut = "execution(* com.ibroximjon.springlearning.springaop.PaymentService.*(..))",
+            returning = "result")
+    public void afterReturningPaymentMethod(Object result) {
+        System.out.println("✅ Metod muvaffaqiyatli yakunlandi! Qaytgan qiymat: " + result);
+    }
+
+
+    @AfterThrowing(
+            pointcut = "execution(* com.ibroximjon.springlearning.springaop.PaymentService.*(..))",
+            throwing = "ex")
+    public void afterThrowingMethod(Throwable ex) {
+        System.out.println("❌ @AfterThrowing: Metodda exception bo‘ldi: " + ex.getMessage());
+    }
+
+    @Around("execution(* com.ibroximjon.springlearning.springaop.PaymentService.*(..))")
+    public Object aroundMethod(ProceedingJoinPoint joinPoint) {
+        System.out.println("🔄 @Around boshlanmoqda...");
+        Object result = null;
+        try {
+            System.out.println("🔹 @Around: Metod: " + joinPoint.getSignature().getName());
+            result = joinPoint.proceed();
+            System.out.println("✅ @Around: Metod muvaffaqiyatli tugadi!");
+        } catch (Throwable ex) {
+            System.out.println("❌ @Around: Xatolik sodir bo‘ldi: " + ex.getMessage());
+        } finally {
+            System.out.println("🔚 @Around tugadi!");
+        }
+        return result;
+    }
+
 }
